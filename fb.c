mniip/vtshot

commit 546957297dd977f4e0d4864fbcebb7185c0a8760

Fix some warnings and semantic mistakes

fb.c
@@ -26,7 +26,7 @@ descriptor fb_init(char const *device)
 	if(0 > ioctl(file_desc, FBIOGET_FSCREENINFO, &fixed_info))
 		die("fb_init: Could not get fixed screen info: %s\n", strerror(errno));
 	say("fb_init: Got fixed screen info: framebuffer size = 0x%x, line length = 0x%x\n", fixed_info.smem_len, fixed_info.line_length);
-	void *map;
+	void *map = NULL;
 	if(do_mmap)
 	{
 		if((void *)-1 == (map = mmap(NULL, fixed_info.smem_len, PROT_READ, MAP_PRIVATE, file_desc, 0)))
@@ -70,7 +70,7 @@ static void read_all(int file_desc, void *buf, size_t size)
 		if(!ret)
 			say("fb_capture: Read ended early at offset 0x%lx\n", lseek(file_desc, 0, SEEK_CUR));
 		size -= ret;
-		buf += ret;
+		buf = (uint8_t *)buf + ret;
 	}
 }
 
@@ -87,8 +87,8 @@ void fb_capture(descriptor const *desc, buffer buf)
 	int height = desc->height;
 	if(width != screen_info.xres || height != screen_info.yres)
 		yell("fb_capture: Device resolution changed\n");
-	uint8_t *map;
-	uint8_t *data;
+	uint8_t *map = NULL;
+	uint8_t *data = NULL;
 	if(do_mmap)
 		map = ((fb_userdata *)desc->userdata)->map;
 	else

gif.c
@@ -81,7 +81,7 @@ void write_gif_sequence(char const *filename, int width, int height, sequence *h
 		rle_free(head->rle, width * height, buf);
 		int x_min = width, x_max = -1, y_min = height, y_max = -1;
 		int x, y;
-		size_t i, j;
+		size_t i;
 		i = 0;
 		for(y = 0; y < height; y++)
 			for(x = 0; x < width; x++, i++)

main.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <string.h>
 #include <getopt.h>
+
 #include <signal.h>
 
 #include "fb.h"
@@ -44,7 +45,7 @@ double fps = 24.0;
 int main(int argc, char *argv[])
 {
 	char const *device = NULL, *output = NULL;
-	int help = 0, benchmark = 0, outtype = 0, seq = 0;
+	int help = 0, benchmark = 0, seq = 0;
 	char const *default_device = "/dev/fb0";
 	init_proc init = &fb_init;
 	cleanup_proc cleanup = &fb_cleanup;

png.c
@@ -17,6 +17,7 @@ static void write_all(int file_desc, void *data, size_t size)
 		if(0 > (ret = write(file_desc, data, size)))
 			die("write_ppm: Error while writing PNG: %s\n", strerror(errno));
 		size -= ret;
+		data = (uint8_t *)data + ret;
 	}
 }
 

ppm.c
@@ -12,13 +12,13 @@
 
 static void write_all(int file_desc, void *data, size_t size)
 {
-	size_t orig_size = size;
 	while(size > 0)
 	{
 		int ret;
 		if(0 > (ret = write(file_desc, data, size)))
 			die("write_ppm: Error while writing PPM: %s\n", strerror(errno));
 		size -= ret;
+		data = (uint8_t *)data + ret;
 	}
 }
 

rle.c
@@ -27,7 +27,7 @@ rle rle_allocate(size_t size, buffer buf)
 		else
 			rle[i++] = base | run << 24;
 	}
-	whisper("rle_allocate: Shrunk %ld bytes to %ld.\n", size * 3, i * sizeof(uint32_t));
+	whisper("rle_allocate: Shrunk %zd bytes to %zd.\n", size * 3, i * sizeof(uint32_t));
 	return realloc(rle, i * sizeof(uint32_t));
 }
 

vcsa.c
@@ -26,7 +26,7 @@ static void read_all(int file_desc, void *buf, size_t size)
 		if(!ret)
 			say("vcsa_capture: Read ended early at offset 0x%lx\n", lseek(file_desc, 0, SEEK_CUR));
 		size -= ret;
-		buf += ret;
+		buf = (uint8_t *)buf + ret;
 	}
 }
 
@@ -40,13 +40,13 @@ descriptor vcsa_init(char const *device)
 	struct stat tty;
 	if(0 > fstat(file_desc, &tty))
 		die("vcsa_init: Could not stat the TTY device: %s\n", strerror(errno));
-	say("vcsa_init: Device id = 0x%04lx\n", tty.st_rdev);
+	say("vcsa_init: Device id = 0x%04x\n", (int)tty.st_rdev);
 	if((tty.st_rdev & 0xFF00) != 0x0400)
-		die("vcsa_init: '%s' is not a TTY device (Expected 0x04xx, got 0x%04lx)\n", device, tty.st_rdev); 
+		die("vcsa_init: '%s' is not a TTY device (Expected 0x04xx, got 0x%04x)\n", device, (int)tty.st_rdev); 
 
 	char vcsa_name[13] = "/dev/vcsa"; // /dev/vcsa + up to 3 digits + nul
 	if(tty.st_rdev & 0xFF)
-		sprintf(vcsa_name, "/dev/vcsa%ld", tty.st_rdev & 0xFF);
+		sprintf(vcsa_name, "/dev/vcsa%d", (int)tty.st_rdev & 0xFF);
 
 	int vcsa_desc = open(vcsa_name, O_RDONLY);
 	if(vcsa_desc < 0)
@@ -144,7 +144,7 @@ void vcsa_capture(descriptor const *desc, buffer buf)
 			if(ch & mask)
 				symbol |= 0x100;
 			if(lower)
-				ch = ch & lower | ch >> 1 & ~lower;
+				ch = (ch & lower) | (ch >> 1 & ~lower);
 			int sym_pixel = op.data[char_distance * symbol + row_distance * dy + (dx >> 3)] & (0x80 >> (dx & 0x7));
 			int color = sym_pixel || (header.cursor_y == chary && header.cursor_x == charx && dy >= op.height - 2) ? (ch >> 8) & 0xF : ch >> 12;
 			*(buf++) = colors_red[color];

commit a9e09a01b330ae892880149f6a8c3e7b4de16ab0

Fix segfault introduced in 546957297dd977f4e0d4864fbcebb7185c0a8760

fb.c
@@ -54,7 +54,8 @@ descriptor fb_init(char const *device)
 void fb_cleanup(descriptor const *desc)
 {
 	say("fb_cleanup: Cleaning up the framebuffer device\n");
-	munmap(((fb_userdata *)desc->userdata)->map, ((fb_userdata *)desc->userdata)->map_length);
+	if(do_mmap)
+		munmap(((fb_userdata *)desc->userdata)->map, ((fb_userdata *)desc->userdata)->map_length);
 	close(((fb_userdata *)desc->userdata)->file_desc);
 	free(desc->userdata);
 	say("fb_cleanup: Done cleaning up\n");